cc05b0f07ac455513d750eef48f961569b432071
Utilizzatore <Utilizzatore@DESKTOP-4DDKKF3>
2024-01-22 21:36:08 +0100

    Aggiungere i file di progetto.

---
+++ RettangoloClasseCasa/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RettangoloClasseCasa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rettangolo r = new Rettangolo();
+
+            r.l1 = float.Parse(Console.ReadLine());
+            r.l2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Area:{0}, perimetro:{1}", r.Area(), r.Perimetro());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RettangoloClasseCasa/Rettangolo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RettangoloClasseCasa
+{
+    internal class Rettangolo
+    {
+        public float l1 { get; set; }
+        public float l2 { get; set; }
+
+        public Rettangolo()
+        {
+        }
+
+        public float Area()
+        {
+            return l1 * l2;
+        }
+
+        public float Perimetro()
+        {
+            return l1 * 2 + l2 * 2;
+        }
+
+    }
+}
